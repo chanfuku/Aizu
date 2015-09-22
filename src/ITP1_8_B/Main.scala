dc4c796eadb0a820d65137cc38d28619f83295df
Eba <k.ebato0318@gmail.com>
2015-09-22 15:42:27 +0900

    apply breakable

--- src/ITP1_8_B/Main.scala
+++ src/ITP1_8_B/Main.scala
@@ -1,8 +1,22 @@
 package ITP1_8_B
 
+import scala.util.control.Breaks.{ break, breakable }
+
 object Main {
+  def main(args: Array[String]) {
+    val sc = new java.util.Scanner(System.in)
+    val nums = scala.collection.mutable.MutableList.empty[List[Int]]
 
-  def main(args: Array[String]) = {
+    breakable {
+      while (true) {
+        val num = sc.next().split("").toList.map(_.toInt)
+        if (num(0) == 0) break
+        nums += num
+      }
+    }
 
+    nums.foreach { num =>
+      println(num.foldLeft(0)((a, b) => a + b))
+    }
   }
 }
