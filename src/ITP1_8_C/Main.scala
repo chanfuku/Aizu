ed65cbfe95997bbb8f20014e8a2834a7e0f57a38
Eba <k.ebato0318@gmail.com>
2015-09-23 18:20:31 +0900

    要件は満たしている

--- src/ITP1_8_C/Main.scala
+++ src/ITP1_8_C/Main.scala
@@ -1,24 +1,18 @@
 package ITP1_8_C
 
-import scala.util.control.Breaks.{ break, breakable }
 import scala.io.Source
 
 object Main {
   def main(args: Array[String]) {
-    val sc = new java.util.Scanner(System.in)
     val lines = scala.collection.mutable.MutableList.empty[String]
 
-    // 未入力でEnter押下された場合どうしたらよいのか調査中
-    breakable {
-      while (sc.hasNext()) {
-        val str = sc.next().map(_.toLower)
-        lines += str
-        if (str.contains(".")) break
+    for (line <- Source.stdin.getLines) {
+      lines += line.map(_.toLower)
+      if (line.length() == 0 || line.contains(".")) {
+        'a' to 'z' foreach { c =>
+          println(s"${c} : " + lines.mkString.count(_ == c))
+        }
       }
-    }
-
-    'a' to 'z' foreach { c =>
-      println(s"${c} : " + lines.mkString.count(_ == c))
     }
   }
 }
